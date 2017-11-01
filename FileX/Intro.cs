2cb34b009a0e1302dc4eccf8c024a2d6c50a42ee
Sefa TOPRAK <sefatprk8@gmail.com>
2017-11-01 15:47:46 +0200

    FileX First Commit

---
+++ FileX/Intro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace FileX
+{
+    class Intro
+    {
+        public Intro()
+        {
+            IntroBaslat();
+        }
+        string[] isimB = { "B", "a", "Ş", "L","ı","Y","o","R" };
+        protected void IntroBaslat()
+        {
+            
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.Write(" \n\n\n\n\n                            ");
+                for (int j = 0; j < 1; j++)
+                {
+                    foreach (var item in isimB)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Beep();
+                        Console.Write(item);
+                        Thread.Sleep(250);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Thread.Sleep(250);
+                    }
+                    Console.Clear();
+                }
+            }
+            Thread.Sleep(1000);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Beep();
+                Thread.Sleep(15);
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Sefa\Desktop\cekbeni.txt"));
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+        }
+    }
+}
---
+++ FileX/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace FileX
+{
+    class Program:Intro
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.SetWindowPosition(0, 0);
+            new Intro();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            int durum = -2;
+            do
+            {
+                Console.Write("Dosya Yazmak İçin 1, Dosya Silmek İçin 2'yi seçin: ");
+                durum = int.Parse(Console.ReadLine());
+                if (durum==1)
+                {
+                    Console.Write("Dosya yolu girin: ");
+                    string yol = Console.ReadLine();
+                    yol = String.Format(@"{0}", yol);
+                    Console.Write("Kaç adet oluşturulsun? : ");
+                    int adet = int.Parse(Console.ReadLine());
+                    Console.Write("Dosya'da ne yazsın? : ");
+                    string yazilacak = Console.ReadLine();
+                    for (int i = 0; i < adet; i++)
+                    {
+                            File.WriteAllText(yol + "\\Dosya" + i + ".txt", yazilacak);
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine("                                  {0}. dosya {1} adıyla oluşturuldu.", i, "Dosya" + i);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("                                  ---------------------###----------------------");
+                        Thread.Sleep(10);
+                    }
+                }
+                else if (durum==2)
+                {
+                    Console.Write("Dosya yolu girin: ");
+                    string yol = Console.ReadLine();
+                    yol = String.Format(@"{0}", yol);
+                    Console.Write("Kaç adet kaldırılsın? : ");
+                    int adet = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < adet; i++)
+                    {
+                        File.Delete(yol + "\\Dosya" + i + ".txt");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("                                  {0}. dosya {1} adıyla kaldırıldı.", yol + "/" + "/Dosya" + i, "Dosya" + i);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("                                  ---------------------###----------------------");
+                    }
+                }
+            } while (durum != -1);
+
+
+           
+            /**/
+
+
+        }
+    }
+}
